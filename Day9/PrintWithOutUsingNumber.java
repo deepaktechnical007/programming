3c889182475f1715024d4d79da00bec28799fe47
DPK <Todeepakbabu@gmail.com>
2026-01-13 11:04:31 +0530

    Programing

---
+++ Day9/PrintWithOutUsingNumber.java
@@ -0,0 +1,8 @@
+public class PrintWithOutUsingNumber {
+    public static void main(String[] args) {
+        
+        for(int i = 'A'/'A';i<='d';i++){
+            System.out.println(i);
+        }
+    }
+}
