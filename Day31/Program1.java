de51c8b82a0d87514a1d54491271b0362928f083
DPK <Todeepakbabu@gmail.com>
2026-02-18 10:43:48 +0530

    Merge remote-tracking branch 'origin/main'

--- Day31/Program1.java
+++ Day31/Program1.java
@@ -4,10 +4,16 @@ public class Program1 {
     public static void main(String[] args) {
         
         int [] a ={16,17,4,3,5,2};
-        
-        for(int i=0;i<a.length;i++){
+        int max = a[a.length-1];
+        System.out.println(max);
+        for(int i=a.length-2;i>=0;i--){
+            if(a[i]>max){
+                max = a[i];
+                System.out.println(max);
+            }
 
-            
+
         }
     }
 }
+
